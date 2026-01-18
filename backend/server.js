15c1db3200c2279e6d056fb8d68eba50433376ca
salaj07 <salaj9877@gmail.com>
2026-01-18 16:23:41 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const { Server } = require("socket.io");
 const generateResponse=require('./src/service/ai.service')
 
 const httpServer = createServer(app);
+
 const io = new Server(httpServer, {
   cors:{
     origin:"http://localhost:5173"
